3358d5d8d8be5256cbeb369754336a2cb6db06c2
eguonzy <eguonzy@yahoo.com>
2021-01-18 06:40:33 +0100

    5

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -72,7 +72,7 @@ router.get("/shop_charge", auth, async (req, res) => {
     tx_ref: Date.now(),
     amount: "50",
     currency: "USD",
-    redirect_url: "http://localhost:3000/payment_done",
+    redirect_url: "https://jumgamarzz/payment_done",
     payment_options: "card",
     meta: {
       consumer_id: user._id,
